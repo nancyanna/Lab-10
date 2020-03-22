20637ee53562b9592e83e69c7860023e85c50630
nancyanna <61117785+nancyanna@users.noreply.github.com>
2020-03-22 10:31:54 -0500

    Add files via upload

---
+++ Music_Player/Interface1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Music_Player
+{
+    interface IAudioPlayer
+    {
+        void Play();
+    }
+}
---
+++ Music_Player/MediaPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Music_Player
+{
+    class MediaPlayer
+    {
+        public void PlayMedia(IAudioPlayer media)
+        {
+            media.Play();
+
+        }
+
+    }
+}
